87f401a8d3237bbe2a204e422f7ff3b2f68dde84
Nikolai Bermudez <nicohhhh1@gmail.com>
2020-04-21 11:33:02 -0500

    Correccion mvn

--- tables.sql
+++ tables.sql
@@ -81,57 +81,7 @@ insert into "User" values (1015475103,'Verbo','verbo','verbo',2);
 insert into type_status values (1,'En revision');
 
 insert into initiative (id,description,area,num_votos,creation_date,user_id,modify_date,type_status_id)
-	values ((select count(*)+1 from "initiative"),'mas parqueaderos','parqueadero',0,(select now()),1015475103,(select now()),'En revision');
+	values ((select count(*)+1 from initiative),'mas parqueaderos','parqueadero',0,(select now()),1015475103,(select now()),1);
 
 insert into Keywords (id,keyword ,idinitiative ) values ((select count(*)+1 from Keywords),'parqueadero',1);
-
--- Consultas
-select
-		i.id ,
-		i.area ,
-		i.creation_date,
-		i.description,
-		i.modify_date,
-		i.num_votos,
-		t.status,
-		k.keyWord
-	from initiative as i
-	inner join type_status as t on i.type_status_id = t.id
-	inner join keywords as k on i.id = k.idinitiative
-	where k.keyword in ('parqueadero');
-
-SELECT
-        i.id,
-        i.num_votos,
-        i.description,
-        i.area,
-        i.creation_date,
-        i.modify_date,
-        i.user_id,
-        u.id as user_id,
-        u.name as user_name,
-        u.email as user_email,
-        u.type_user as user_type_user,
-        u.password as user_password,
-        ts.id,
-        ts.status
-        FROM Initiative as i
-        INNER JOIN "User" as u on u.id = i.user_id
-        INNER JOIN Type_Status as ts on ts.status = i.type_status_id;
-
-select
-		i.id,
-		c.initiative
-		from "Comments" as c
-		inner join initiative as i on c.initiative =i.id ;
-
-select
-		u.id,
-		c.user
-		from "Comments" as c
-		inner join "User" as u on c."user" = u.id ;
-
-select
-		i.area
-		from initiative as i;
 

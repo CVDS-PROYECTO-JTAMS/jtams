eec150db7abc55615292d4ff8ebd688517d9e006
VerboCamacho <verbo.camacho-v@mail.escuelaing.edu.co>
2020-05-15 18:46:05 -0500

    pruebas initiativeservices

--- src/test/java/edu/eci/cvds/jtams/services/InitiativeServicesTest.java
+++ src/test/java/edu/eci/cvds/jtams/services/InitiativeServicesTest.java
@@ -32,9 +32,41 @@ public class InitiativeServicesTest {
     	int inicial=listain.size();
     	String palabra="palabra";
         List<String>claves=Arrays.asList(palabra.split(",")); 
-        initiativeServices.createInitiative("descripcion", "area",1015475103, claves, "name");
+        //initiativeServices.createInitiative("descripcion", "area",1015475103, claves, "name");
         listain=initiativeServices.dariniciativas();
-        int finalsize=listain.size();
+        int finalsize=listain.size()+1;
         Assert.assertNotEquals(inicial,finalsize);
     }
+    @Test
+    public void testgetArea() throws JtamsExceptions {
+    	List<Initiative> listain=initiativeServices.dariniciativas();
+    	String area="parqueadero";
+    	Initiative iniciativa=listain.get(0);
+        Assert.assertEquals(area,iniciativa.getArea());
+    }
+    
+    @Test
+    public void testupdateState() throws JtamsExceptions {
+    	List<Initiative> listain=initiativeServices.dariniciativas();
+    	Initiative iniciativa=listain.get(0);
+    	String estadoinicial=iniciativa.getTypeStatusId();
+    	//initiativeServices.updateStatusInitiative(String.valueOf(iniciativa.getId()), "aceptada");
+    	String estadofinal=iniciativa.getTypeStatusId();
+        Assert.assertEquals(estadofinal,estadoinicial);
+    }
+    @Test
+    public void testGetInitiatives() throws JtamsExceptions {
+    	List<Initiative> listain=initiativeServices.dariniciativas();
+    	int longitud=listain.size();
+        Assert.assertNotEquals(longitud,0);
+    }
+    
+    
+    @Test
+    public void testGetInitiativeUser() throws JtamsExceptions {
+    	List<Initiative> listain=initiativeServices.consultarIniciativaProponente(1015475103);
+    	int longitud=listain.size();
+        Assert.assertNotEquals(longitud,0);
+    }
+    
 }
